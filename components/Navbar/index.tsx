f6a156eb4e23bf16832bd369a772222ac64ffa0b
stormymayday <stormymayday7@gmail.com>
2024-11-27 17:40:06 -0800

    Initial Setup

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,4 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { links } from "@/data";
+import logo from "@/assets/logo.svg";
+import Image from "next/image";
+
 function Navbar() {
-    return <div>Navbar</div>;
+    const [showLinks, setShowLinks] = useState(false);
+
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    };
+
+    return (
+        <nav>
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Image src={logo} alt="logo" className="logo" />
+                    <button className="nav-toggle" onClick={toggleLinks}>
+                        <FaBars />
+                    </button>
+                </div>
+                {showLinks && (
+                    <div className="links-container">
+                        <ul className="link">
+                            {links.map((link) => {
+                                const { id, url, text } = link;
+                                return (
+                                    <li key={id}>
+                                        <a href={url}>{text}</a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </nav>
+    );
 }
 export default Navbar;
