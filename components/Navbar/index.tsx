75156505a991b686a3dc250204038c6e10010539
stormymayday <stormymayday7@gmail.com>
2024-11-27 18:54:54 -0800

    Text Logo

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,8 +4,6 @@ import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import { links, social } from "@/data";
-import logo from "@/assets/logo.svg";
-import Image from "next/image";
 
 function Navbar() {
     const [showLinks, setShowLinks] = useState(false);
@@ -27,7 +25,9 @@ function Navbar() {
         <nav>
             <div className="nav-center">
                 <div className="nav-header">
-                    <Image src={logo} alt="logo" className="logo" />
+                    <h4 className="logo">
+                        Your <span> Logo</span>
+                    </h4>
                     <button className="nav-toggle" onClick={toggleLinks}>
                         {showLinks ? <ImCross /> : <FaBars />}
                     </button>
